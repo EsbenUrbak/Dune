484fd6bf0d0893f19c9db99831db047e5fcf77a4
k-o-r-t-o <jg2909@columbia,edu>
2015-06-10 22:49:47 -0400

    A bit of clean up in the code: - took the map reader out of the map class and into Resources - moved the image manipulation functions into Util->ImageHandler - used the path object and deleted the previous arrays that handled the paths coordinates - moved the path array from the PlayScreen to the squad - set the squad selection as a property of the squad - moved the squad animation updates from the PlayScreen to the squad

--- Game/src/com/baseframework/screen/PlayScreen.java
+++ Game/src/com/baseframework/screen/PlayScreen.java
@@ -2,12 +2,9 @@ package com.baseframework.screen;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.event.ComponentEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.io.IOException;
-import java.util.ArrayList;
 
 import com.baseframework.game.main.*;
 import com.dune.entities.Path;
@@ -27,21 +24,16 @@ public class PlayScreen extends GameScreen{
 	public static Map map;
 
 	// Graphics objects
-	Graphics2D g2;
+	public Graphics2D g2;
 	
 	// numerical variables
 	public static int screenSizeX = 800;
 	public static int screenSizeY = 480;
-	public static final int INITIALSCREENX = 0;
-	public static final int INITIALSCREENY = 0;
 	
-	//Mouse elements
-	public static ArrayList<Integer> pathXPoints = new ArrayList<Integer>();
-	public static ArrayList<Integer> pathYPoints = new ArrayList<Integer>();
-	public static ArrayList<Integer> activeList = new ArrayList<Integer>();
-	boolean squadSelected;
-	int active = 1;
-	Path pathPoint;
+	private final float SQUAD_TOPX = 300f; 
+	private final float SQUAD_TOPY = 200f;
+	public static final int SCREEN_X = 0;
+	public static final int SCREEN_Y = 0;
 
 	@Override
 	public void init() {
@@ -49,15 +41,9 @@ public class PlayScreen extends GameScreen{
 		
 		MainHolder.thegame.setDimensions(screenSizeX, screenSizeY);
 		
-		viewframe = new ViewFrame((float) INITIALSCREENX, (float) INITIALSCREENY, screenSizeX, screenSizeY);
-		squad = new Squad();
-		
-		try {
-			map = new Map(Resources.URLmap1);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		viewframe = new ViewFrame((float) SCREEN_X, (float) SCREEN_Y, screenSizeX, screenSizeY);
+		squad = new Squad(SQUAD_TOPX, SQUAD_TOPY, false);	
+		map = new Map(Resources.map1);
 		
 		viewframe.setBoundX(map.getWidth(true));
 		viewframe.setBoundY(map.getHeight(true));
@@ -66,23 +52,10 @@ public class PlayScreen extends GameScreen{
 
 	@Override
 	public void update(float delta) {
-		// Updating the screen, squad, and zone of the map to be displayed
 		viewframe.update(delta);
 		squad.update(delta);
-		
 		map.update((int) viewframe.getFrameX(), (int) viewframe.getFrameY());
-		
-		Resources.selectAnim.update(delta);
-		squad.getCurrentAnim().update(delta);
-		
 		//updateTiles();
-		
-		/* update squad properties
-		if (squadSelected) {
-			squadCurrent = Resources.squadClickedImagine;
-		} else {
-			squadCurrent = Resources.squadImagine;
-		}*/
 	}
 
 	@Override
@@ -104,36 +77,33 @@ public class PlayScreen extends GameScreen{
 		g2 = (Graphics2D) g;
 	    g2.setStroke(Resources.strokeSize);	
 
-		if(pathXPoints.size()>0){
-			for ( int i = 0; i < pathXPoints.size(); ++i ) {
+		if(!squad.paths.isEmpty()){
+			for ( int i = 0; i < squad.paths.size(); ++i ) {
 				if(i==0){
 					g2.drawLine((int) (squad.getTopX()+ (float)squad.getxImagine()/2f - viewframe.getFrameX()), 
 								(int) (squad.getTopY()+ (float)squad.getyImagine()/2f - viewframe.getFrameY()), 
-								pathXPoints.get(0)- (int) viewframe.getFrameX(), 
-								pathYPoints.get(0)- (int) viewframe.getFrameY());
+								squad.paths.get(0).getX()- (int) viewframe.getFrameX(), 
+								squad.paths.get(0).getY()- (int) viewframe.getFrameY());
 				
-					g.drawOval(pathXPoints.get(0)-1 - (int) viewframe.getFrameX(),pathYPoints.get(0)-1- (int) viewframe.getFrameY(),3,3);
-					g.drawOval(pathXPoints.get(0)-7- (int) viewframe.getFrameX(),pathYPoints.get(0)-7- (int) viewframe.getFrameY(),15,15);
+					g.drawOval(squad.paths.get(0).getX()-1 - (int) viewframe.getFrameX(),squad.paths.get(0).getY()-1- (int) viewframe.getFrameY(),3,3);
+					g.drawOval(squad.paths.get(0).getX()-7- (int) viewframe.getFrameX(),squad.paths.get(0).getY()-7- (int) viewframe.getFrameY(),15,15);
 				}else{
-					g2.drawLine(pathXPoints.get(i-1)- (int) viewframe.getFrameX(), pathYPoints.get(i-1)- (int) viewframe.getFrameY(),
-								pathXPoints.get(i)- (int) viewframe.getFrameX(), pathYPoints.get(i)- (int) viewframe.getFrameY());	
-					g.drawOval(pathXPoints.get(i)-1- (int) viewframe.getFrameX(),pathYPoints.get(i)-1- (int) viewframe.getFrameY(),3,3);
-					g.drawOval(pathXPoints.get(i)-5- (int) viewframe.getFrameX(),pathYPoints.get(i)-5- (int) viewframe.getFrameY(),11,11);
+					g2.drawLine(squad.paths.get(i-1).getX()- (int) viewframe.getFrameX(), squad.paths.get(i-1).getY()- (int) viewframe.getFrameY(),
+							squad.paths.get(i).getX()- (int) viewframe.getFrameX(), squad.paths.get(i).getY()- (int) viewframe.getFrameY());	
+					g.drawOval(squad.paths.get(i-1).getX()-1- (int) viewframe.getFrameX(),squad.paths.get(i-1).getY()-1- (int) viewframe.getFrameY(),3,3);
+					g.drawOval(squad.paths.get(i).getX()-5- (int) viewframe.getFrameX(),squad.paths.get(i).getY()-5- (int) viewframe.getFrameY(),11,11);
 				}	
 			}
 		}
 	}
 	
-	private void renderSquad(Graphics g) {
-		
-		if(squadSelected){
+	private void renderSquad(Graphics g) {	
+		if(squad.isSelected()){
 			Resources.selectAnim.render(g, 
 					(int) (squad.getTopX() - viewframe.getFrameX() + (squad.xImagine - Resources.selectAnim.getCurrentWidth())/2), 
 					(int) (squad.getTopY() - viewframe.getFrameY()) + squad.yImagine - Resources.selectAnim.getCurrentHeight()/2 - 5);			
-		}
-		
+		}	
 		squad.getCurrentAnim().render(g, (int) (squad.getTopX() - viewframe.getFrameX()), (int) (squad.getTopY()-viewframe.getFrameY()));			
-
 	}
 	
 	
@@ -146,33 +116,27 @@ public class PlayScreen extends GameScreen{
 		xPos = e.getX();
 		yPos = e.getY();
 
-		// adding path to the list but only if outside of squad and if the squad
-		// is selected
-
 		// Logic to check whether it was within the rectangle (in relative coordinate)
 		if (squad.rect.contains(xPos+ (int) viewframe.getFrameX(), yPos+ (int) viewframe.getFrameY())) {
-			squadSelected = !squadSelected;
+			squad.setSelected(!squad.isSelected());
 		}
-		
-		if(squadSelected){
+
+		// adding path to the list but only if outside of squad and if the squad is selected
+		if(squad.isSelected()){
 			if (!squad.rect.contains(xPos+ (int) viewframe.getFrameX(), yPos+ (int) viewframe.getFrameY())){
 				
-				if(pathXPoints.size() ==0){
+				if(squad.paths.isEmpty()){
 					addNewPath = true;
-				} else if (pathXPoints.get(pathXPoints.size()-1) != xPos+ (int) viewframe.getFrameX() || 
-						pathYPoints.get(pathYPoints.size()-1) != yPos + (int) viewframe.getFrameY()) {
+				} else if (squad.paths.get(squad.paths.size()-1).getX() != xPos+ (int) viewframe.getFrameX() || 
+						squad.paths.get(squad.paths.size()-1).getY() != yPos + (int) viewframe.getFrameY()) {
 					addNewPath = true;
 				}
 				
 				if(addNewPath){
-					pathXPoints.add(xPos+(int) viewframe.getFrameX());
-					pathYPoints.add(yPos+(int) viewframe.getFrameY());
-					activeList.add(active);
+					squad.paths.add(new Path(xPos+(int) viewframe.getFrameX(), yPos+(int) viewframe.getFrameY(), true));
 				}
 			}	
 		}
-
-
 		
 	}
 
@@ -255,30 +219,5 @@ public class PlayScreen extends GameScreen{
 	public static ViewFrame getViewFrame() {
 		return viewframe;
 	}
-
-	public static ArrayList<Integer> getPathXPoints() {
-		return pathXPoints;
-	}
-
-	public static void setPathXPoints(ArrayList<Integer> newPathXPoints) {
-		pathXPoints = newPathXPoints;
-	}
-
-	public static ArrayList<Integer> getPathYPoints() {
-		return pathYPoints;
-	}
-
-	public static void setPathYPoints(ArrayList<Integer> newPathYPoints) {
-		pathYPoints = newPathYPoints;
-	}
-
-	public static ArrayList<Integer> getActiveList() {
-		return activeList;
-	}
-
-	public static void setActiveList(ArrayList<Integer> newActiveList) {
-		activeList = newActiveList;
-	}
-
 
 }
